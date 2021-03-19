596b086ce47ad8347e01256c18f77cddb8f8eb7a
FacundoPonce <mrfacu125@gmail.com>
2021-03-19 17:57:58 -0300

    Dot product working on vec3.

--- TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/TestOnConsole.cs
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/TestOnConsole.cs
@@ -8,14 +8,24 @@ public class TestOnConsole : MonoBehaviour
 {
     void Start()
     {
-        Vec3 vectorVec3 = new Vec3(2,2,2);
-        Vector3 vectororiginal = new Vec3(2,2,2);
+        Vec3 vec3_1 = new Vec3(2, 2, 2);
+        Vec3 vecToDot = new Vec3(4, 6, 8);
+        Vector3 vectororiginal = new Vec3(2, 2, 2);
 
+        #region //Visualizar vector original y nuestro
         Debug.Log("Vector Original: " + vectororiginal);
-        Debug.Log("Vector Vec3: " + vectorVec3);
+        Debug.Log("Vector Vec3: " + vec3_1);
+        #endregion
 
+        #region //Visualizar magnitud vector original y nuestro
         Debug.Log("Vector Original magnitud: " + vectororiginal.magnitude);
-        Debug.Log("Vector Vec3 magnitud: " + vectorVec3.magnitude);
+        Debug.Log("Vector Vec3 magnitud: " + vec3_1.magnitude);
+        #endregion
+
+        #region//Visualizar producto punto vector original y nuestro
+        Debug.Log("Producto punto Original vector: " + Vector3.Dot(vectororiginal, vecToDot));
+        Debug.Log("Producto punto Vec3: " + Vec3.Dot(vec3_1, vecToDot));
+        #endregion
     }
 
     void Update()
