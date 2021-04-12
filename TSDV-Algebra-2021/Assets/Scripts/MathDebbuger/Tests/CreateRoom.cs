91b801c46c3f9eca5c8f18529a3c46aed1bb9115
FacundoPonce <mrfacu125@gmail.com>
2021-04-12 10:36:43 -0300

    Plane exercise working.

---
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/CreateRoom.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateRoom : MonoBehaviour
+{
+    public Plane up;
+    public Plane down;
+    public Plane left;
+    public Plane rigth;
+    public Plane front;
+    public Plane back;
+
+    [SerializeField] Vector3 pointToCheck;
+    [SerializeField] GameObject cubePoint;
+
+    private bool upBool;
+    private bool downBool;
+    private bool leftBool;
+    private bool rightBool;
+    private bool frontBool;
+    private bool backBool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 point1 = new Vector3(-5,-5,-5);
+        Vector3 point2 = new Vector3( 5,-5,-5);
+        Vector3 point3 = new Vector3( 5, 5,-5);
+        Vector3 point4 = new Vector3(-5, 5,-5);
+        Vector3 point5 = new Vector3(-5,-5, 5);
+        Vector3 point6 = new Vector3( 5,-5, 5);
+        Vector3 point7 = new Vector3( 5, 5, 5);
+        Vector3 point8 = new Vector3(-5, 5, 5);
+
+        front = new Plane(point5, point7);
+        back = new Plane(point1, point3);
+        up = new Plane(point4, point7);
+        down = new Plane(point1, point6);
+        left = new Plane(point1, point8);
+        rigth = new Plane(point2, point7);
+    }
+
+    void Update()
+    {
+        upBool = up.GetSide(cubePoint.transform.position);
+        downBool = down.GetSide(cubePoint.transform.position);
+        leftBool = left.GetSide(cubePoint.transform.position);
+        rightBool = rigth.GetSide(cubePoint.transform.position);
+        frontBool = front.GetSide(cubePoint.transform.position);
+        backBool = back.GetSide(cubePoint.transform.position);
+
+        if ( upBool && backBool && leftBool && rightBool && downBool && frontBool)
+        {
+            Debug.Log("El cubo esta dentro de la habitacion");
+        }
+        else
+        {
+            Debug.Log("El cubo esta fuera de la habitacion");
+        }
+    }
+}
---
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/myPlane.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+namespace CustomMath
+{
+    [Serializable]
+    public struct myPlane
+    {
+        #region Variables
+        public Vec3 normal;
+        public Vec3 point;
+        public float distance;
+        public Vec3 a;
+        public Vec3 b;
+        public Vec3 c;
+        #endregion
+
+        #region Functions
+       // public myPlane(Vec3 inNormal, Vec3 inPloint)
+       // {
+       //     normal = inNormal;
+       //     point = inPloint;
+       // }
+       // public myPlane(Vec3 inNormal, float d)
+       // {
+       //     normal = inNormal;
+       //     distance = d;
+       // }
+       // public myPlane(Vec3 inA, Vec3 inB, Vec3 inC)
+       // {
+       //     a = inA;
+       //     b = inB;
+       //     c = inC;
+       // }
+        #endregion
+    }
+}
