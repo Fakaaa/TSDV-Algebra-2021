3bd715377081247d2a6e8be4eb3a3ac4243f1a5a
FacundoPonce <mrfacu125@gmail.com>
2021-04-20 22:47:42 -0300

    The frustrum now can move.
    xd

--- TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/CreateFrustrum.cs
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/CreateFrustrum.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CustomMath;
+using MathDebbuger;
 
 public class CreateFrustrum : MonoBehaviour
 {
@@ -12,57 +14,72 @@ public class CreateFrustrum : MonoBehaviour
     public Plane bot;
 
     [SerializeField] GameObject prefabPoint;
-    [SerializeField] GameObject [] pointsFrustrum;
+    [SerializeField] GameObject[] pointsFrustrum;
 
-    [SerializeField] Vector3 point1;
-    [SerializeField] Vector3 point2;
-    [SerializeField] Vector3 point3;
-    [SerializeField] Vector3 point4;
-    [SerializeField] Vector3 point5;
-    [SerializeField] Vector3 point6;
-    [SerializeField] Vector3 point7;
-    [SerializeField] Vector3 point8;
+    [SerializeField] public List<Vec3> points;
+
+    [SerializeField] public List<int> objectsInScene;
+
+    public int initID;
     void Start()
     {
-        point1 = new Vector3(-2,-1,-1);
-        point2 = new Vector3( 2,-1,-1);
-        point3 = new Vector3( 2, 1,-1);
-        point4 = new Vector3(-2, 1,-1);
-        point5 = new Vector3(-5,-5, 15);
-        point6 = new Vector3( 5,-5, 15);
-        point7 = new Vector3( 5, 5, 15);
-        point8 = new Vector3(-5, 5, 15);
+        //for (int i = 0; i < points.Count; i++)
+        //{
+        //    Vector3Debugger.AddVector(points[i], i.ToString());
+        //    Vector3Debugger.UpdateColor(i.ToString(), Color.white);
+        //
+        //}
+        //Vector3Debugger.EnableEditorView();
 
-        front = new Plane(point5, point7);
-        back = new Plane(point1, point3);
-        top = new Plane(point4, point7);
-        bot = new Plane(point1, point6);
-        left = new Plane(point1, point8);
-        right = new Plane(point2, point7);
+
+        objectsInScene.Clear();
+        initID = -1;
+        objectsInScene.Add(initID);
+
+        front = new Plane(points[4], points[6]);
+        back = new Plane(points[0], points[2]);
+        top = new Plane(points[3], points[6]);
+        bot = new Plane(points[0], points[5]);
+        left = new Plane(points[0], points[7]);
+        right = new Plane(points[1], points[6]);
 
         for (int i = 0; i < pointsFrustrum.Length; i++)
         {
-            pointsFrustrum[i] = Instantiate(prefabPoint);
+            pointsFrustrum[i] = Instantiate(prefabPoint, points[i], transform.rotation, transform);
+            pointsFrustrum[i].name = "Point" + i.ToString();
         }
     }
 
     void Update()
     {
-        if(pointsFrustrum[0]!= null)
-        pointsFrustrum[0].gameObject.transform.position = point1;
-        if(pointsFrustrum[1]!= null)
-        pointsFrustrum[1].gameObject.transform.position = point2;
-        if(pointsFrustrum[2]!= null)
-        pointsFrustrum[2].gameObject.transform.position = point3;
-        if(pointsFrustrum[3]!= null)
-        pointsFrustrum[3].gameObject.transform.position = point4;
-        if(pointsFrustrum[4]!= null)
-        pointsFrustrum[4].gameObject.transform.position = point5;
-        if(pointsFrustrum[5]!= null)
-        pointsFrustrum[5].gameObject.transform.position = point6;
-        if(pointsFrustrum[6]!= null)
-        pointsFrustrum[6].gameObject.transform.position = point7;
-        if(pointsFrustrum[7]!= null)
-        pointsFrustrum[7].gameObject.transform.position = point8;
+        //Vec3 papaPos = new Vec3(transform.position + transform.forward);
+
+        //Debug.DrawLine(points[0] + papaPos, points[1] + papaPos, Color.red);// Back
+        //Debug.DrawLine(points[1] + papaPos, points[2] + papaPos, Color.red);// Back
+        //Debug.DrawLine(points[2] + papaPos, points[3] + papaPos, Color.red);// Back
+        //Debug.DrawLine(points[3] + papaPos, points[0] + papaPos, Color.red);// Back
+        ////----------
+        //Debug.DrawLine(points[4] + papaPos, points[5] + papaPos, Color.green);// Front
+        //Debug.DrawLine(points[5] + papaPos, points[6] + papaPos, Color.green);// Front
+        //Debug.DrawLine(points[6] + papaPos, points[7] + papaPos, Color.green);// Front
+        //Debug.DrawLine(points[7] + papaPos, points[4] + papaPos, Color.green);// Front
+        ////----------
+        //Debug.DrawLine(points[0] + papaPos, points[4] + papaPos, Color.yellow);// Sides
+        //Debug.DrawLine(points[3] + papaPos, points[7] + papaPos, Color.yellow);// Sides
+        //Debug.DrawLine(points[1] + papaPos, points[5] + papaPos, Color.yellow);// Sides
+        //Debug.DrawLine(points[2] + papaPos, points[6] + papaPos, Color.yellow);// Sides
+
+        //for (int i = 0; i < points.Count; i++)
+        //{
+        //    points[i] = new Vec3(pointsFrustrum[i].transform.position);
+        //    Vector3Debugger.UpdatePosition(i.ToString(), points[i]);
+        //}
+    }
+
+    public void SetVec3(float newx, float newy, float newz, ref Vec3 vec)
+    {
+        vec.x = newx;
+        vec.y = newy;
+        vec.z = newz;
     }
 }
---
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/FindPoints.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FindPoints : MonoBehaviour
+{
+    [SerializeField] GameObject pointRef;
+
+    private LineRenderer myLine;
+    void Start()
+    {
+        myLine = gameObject.GetComponent<LineRenderer>();
+    }
+
+    void Update()
+    {
+        pointRef = FindObjectOfType<CreateFrustrum>().gameObject;
+
+        myLine.SetPosition(0, transform.position);
+        myLine.SetPosition(1, pointRef.transform.position);
+    }
+}
