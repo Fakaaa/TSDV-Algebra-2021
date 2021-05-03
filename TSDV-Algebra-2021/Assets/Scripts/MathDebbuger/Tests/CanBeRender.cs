ea3a948589e6aec22256a97b0c09fff27730ddd8
FacundoPonce <mrfacu125@gmail.com>
2021-05-03 09:32:46 -0300

    Setting up the objects that will render with the frustrum.

---
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/CanBeRender.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanBeRender : MonoBehaviour
+{
+
+    public delegate void ICanBeRender();
+
+    public ICanBeRender stillRendering;
+
+    public MeshRenderer myMesh;
+    private void Start()
+    {
+        myMesh = gameObject.GetComponent<MeshRenderer>();
+    }
+
+    public void RenderMe()
+    {
+        stillRendering?.Invoke();
+    }
+}
--- TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/CreateFrustrum.cs
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/CreateFrustrum.cs
@@ -18,23 +18,15 @@ public class CreateFrustrum : MonoBehaviour
 
     [SerializeField] public List<Vec3> points;
 
-    [SerializeField] public List<int> objectsInScene;
+    [SerializeField] public List<CanBeRender> objectsInScene;
 
     public int initID;
     void Start()
     {
-        //for (int i = 0; i < points.Count; i++)
-        //{
-        //    Vector3Debugger.AddVector(points[i], i.ToString());
-        //    Vector3Debugger.UpdateColor(i.ToString(), Color.white);
-        //
-        //}
-        //Vector3Debugger.EnableEditorView();
-
-
         objectsInScene.Clear();
-        initID = -1;
-        objectsInScene.Add(initID);
+        objectsInScene.Add(FindObjectOfType<CanBeRender>());
+        
+
 
         front = new Plane(points[4], points[6]);
         back = new Plane(points[0], points[2]);
@@ -52,34 +44,17 @@ public class CreateFrustrum : MonoBehaviour
 
     void Update()
     {
-        //Vec3 papaPos = new Vec3(transform.position + transform.forward);
-
-        //Debug.DrawLine(points[0] + papaPos, points[1] + papaPos, Color.red);// Back
-        //Debug.DrawLine(points[1] + papaPos, points[2] + papaPos, Color.red);// Back
-        //Debug.DrawLine(points[2] + papaPos, points[3] + papaPos, Color.red);// Back
-        //Debug.DrawLine(points[3] + papaPos, points[0] + papaPos, Color.red);// Back
-        ////----------
-        //Debug.DrawLine(points[4] + papaPos, points[5] + papaPos, Color.green);// Front
-        //Debug.DrawLine(points[5] + papaPos, points[6] + papaPos, Color.green);// Front
-        //Debug.DrawLine(points[6] + papaPos, points[7] + papaPos, Color.green);// Front
-        //Debug.DrawLine(points[7] + papaPos, points[4] + papaPos, Color.green);// Front
-        ////----------
-        //Debug.DrawLine(points[0] + papaPos, points[4] + papaPos, Color.yellow);// Sides
-        //Debug.DrawLine(points[3] + papaPos, points[7] + papaPos, Color.yellow);// Sides
-        //Debug.DrawLine(points[1] + papaPos, points[5] + papaPos, Color.yellow);// Sides
-        //Debug.DrawLine(points[2] + papaPos, points[6] + papaPos, Color.yellow);// Sides
-
-        //for (int i = 0; i < points.Count; i++)
-        //{
-        //    points[i] = new Vec3(pointsFrustrum[i].transform.position);
-        //    Vector3Debugger.UpdatePosition(i.ToString(), points[i]);
-        //}
+        
     }
-
     public void SetVec3(float newx, float newy, float newz, ref Vec3 vec)
     {
         vec.x = newx;
         vec.y = newy;
         vec.z = newz;
+    }
+
+    public void OnRender()
+    {
+        
     }
 }
