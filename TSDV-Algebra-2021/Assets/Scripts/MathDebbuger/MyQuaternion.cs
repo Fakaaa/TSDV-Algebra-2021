37bef5a2f404ca6b5ea97e1b525b12575ab89ca0
FacundoPonce <mrfacu125@gmail.com>
2021-06-07 11:08:33 -0300

    Quaternion multiply almost done.
    It seems fine, but we need to test.

--- TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/MyQuaternion.cs
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/MyQuaternion.cs
@@ -3,6 +3,7 @@ using System;
 
 namespace CustomMath
 {
+    [Serializable]
     public struct MyQuaternion : IEquatable<MyQuaternion>
     {
         public float x;
@@ -30,12 +31,6 @@ namespace CustomMath
             this.x = x; this.y = y;
             this.z = z; this.w = w;
         }
-
-        public MyQuaternion(Vec3 v3)
-        {
-            x = v3.x; y = v3.y;
-            z = v3.z; w = 1;
-        }
         public static MyQuaternion identity
         {
             get
@@ -43,7 +38,7 @@ namespace CustomMath
                 return identityQuat;
             }
         }
-        public float Dot(MyQuaternion a, MyQuaternion b)
+        public static float Dot(MyQuaternion a, MyQuaternion b)
         {
             return (a.x * b.x + a.y * b.y + a.z * b.z + a.w * b.w);
         }
@@ -55,9 +50,43 @@ namespace CustomMath
         }
         public static MyQuaternion operator *(MyQuaternion a, MyQuaternion b)
         {
+            #region Rules
+            //ixi = -1;
+            //jxj = -1;
+            //kxk = -1;
+            //ijk = -1;
+            #endregion
+            float awXbw = a.w * b.w;
+            float awXbx = a.w * b.x;
+            float awXby = a.w * b.y;
+            float awXbz = a.w * b.z;
+
+            float axXbw = a.x * b.w;
+            float axXbx = a.x * b.x; 
+            float axXby = a.x * b.y; 
+            float axXbz = a.x * b.z; 
+
+            float ayXbw = a.y * b.w;
+            float ayXbx = a.y * b.x; 
+            float ayXby = a.y * b.y; 
+            float ayXbz = a.y * b.z; 
+
+            float azXbw = a.z * b.w;
+            float azXbx = a.z * b.x; 
+            float azXby = a.z * b.y; 
+            float azXbz = a.z * b.z; 
 
 
-            return new MyQuaternion();
+            float rowRealResult = (awXbw - axXbx - ayXby - azXbz);//* w 
+            float rowIResult = (awXbx + axXbw + ayXbz - azXby);//* i
+            float rowJResult = (awXby - axXbz + ayXbw + azXbx);//* j
+            float rowKResult = (awXbz + axXby - ayXbx + azXbw);//* k
+
+            return new MyQuaternion(rowIResult, rowJResult, rowKResult, rowRealResult);
+        }
+        public override string ToString()
+        {
+            return "X = " + x.ToString() + "   Y = " + y.ToString() + "   Z = " + z.ToString() + "   W = " + w.ToString();
         }
 
         #region Internals
--- TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/MyQuaternion Tests/TestQuaternion.cs
+++ TSDV-Algebra-2021/Assets/Scripts/MathDebbuger/Tests/MyQuaternion Tests/TestQuaternion.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using CustomMath;
 
 public class TestQuaternion : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Quaternion unityQuaternion;
+    private Quaternion unityQuaternion2;
+    private MyQuaternion myQuaternion;
+    private MyQuaternion myQuaternion2;
+
     void Start()
     {
-        
+        unityQuaternion = new Quaternion(0, 0, 0, 1);
+        unityQuaternion2 = new Quaternion(1, 0, 0, 1);
+        myQuaternion = new MyQuaternion(0, 0, 0, 1);
+        myQuaternion2 = new MyQuaternion(1, 0, 0, 1);
+
+        Debug.Log("Unity quaternion:" + unityQuaternion.ToString());
+        Debug.Log("My quaternion:" + myQuaternion.ToString());
+        Debug.Log("--------------------------------");
+        Debug.Log("Unity quaternion DOT:" + Quaternion.Dot(unityQuaternion,unityQuaternion2));
+        Debug.Log("My quaternion DOT:" + MyQuaternion.Dot(myQuaternion, myQuaternion2));
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
